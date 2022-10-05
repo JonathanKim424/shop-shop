9eaf3977d63771f91fefcaa8449f9eb688bf1072
Jonathan Kim <vizard424@yahoo.com>
2022-10-05 13:04:40 -0600

    added success page for post checkout

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+import Success from './pages/Success';
 import { StoreProvider } from './utils/GlobalState';
 import OrderHistory from './pages/OrderHistory';
 
@@ -49,6 +50,7 @@ function App() {
               <Route path="/signup" element={<Signup />} />
               <Route path="/orderHistory" element={<OrderHistory />} />
               <Route path="/products/:id" element={<Detail />} />
+              <Route path="/success" element={<Success />} />
               <Route path="*" element={<NoMatch />} />
             </Routes>
           </StoreProvider>
---
+++ client/src/pages/Success.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useMutation } from 'react';
+import Jumbotron from '../components/Jumbotron';
+import { ADD_ORDER } from '../utils/mutations';
+import { idbPromise } from '../utils/helpers';
+
+function Success() {
+    return (
+        <div>
+            <Jumbotron>
+                <h1>Success!</h1>
+                <h2>
+                    Thank you for your purchase!
+                </h2>
+                <h2>
+                    You will now be redirected to the homepage
+                </h2>
+            </Jumbotron>
+        </div>
+    );
+};
+
+export default Success;
