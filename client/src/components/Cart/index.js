ca47d3bf7a53dcb385e3efb8e881a3458f52b8c9
Jonathan Kim <vizard424@yahoo.com>
2022-10-05 12:13:37 -0600

    added stripe integration to front end

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
 import CartItem from '../CartItem';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import Auth from '../../utils/auth';
 import { idbPromise } from '../../utils/helpers';
+import { QUERY_CHECKOUT } from '../../utils/queries';
 import './style.css';
 
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 const Cart = () => {
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
     const [state, dispatch] = useStoreContext();
     useEffect(() => {
         async function getCart() {
@@ -17,15 +23,37 @@ const Cart = () => {
             getCart();
         }
     }, [state.cart.length, dispatch]);
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
-    };
+    }
     function calculateTotal() {
         let sum = 0;
         state.cart.forEach(item => {
             sum += item.price * item.purchaseQuantity;
         });
         return sum.toFixed(2);
+    }
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        console.log(productIds);
+
+        getCheckout({
+            variables: { products: productIds }
+        });
     }
     if (!state.cartOpen) {
         return (
@@ -47,7 +75,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
